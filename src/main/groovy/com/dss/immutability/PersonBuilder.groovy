469e102aeabfdbb669d57d72885a70319e53b1e2
Matt Stine <mstine@gopivotal.com>
2014-04-05 15:47:53 -0400

    eliminate duplication

--- src/main/groovy/com/dss/immutability/PersonBuilder.groovy
+++ src/main/groovy/com/dss/immutability/PersonBuilder.groovy
@@ -2,49 +2,36 @@ package com.dss.immutability
 
 class PersonBuilder extends BuilderSupport {
 
-    def personExpando
-    def addressExpando
-    def homePhoneExpando
-    def mobilePhoneExpando
+    def expandos
 
     PersonBuilder() {
-        addressExpando = new Expando()
-        personExpando = new Expando()
-        homePhoneExpando = new Expando()
-        mobilePhoneExpando = new Expando()
+        expandos = ["person": new Expando(),
+                    "address": new Expando(),
+                    "homePhone": new Expando(),
+                    "mobilePhone": new Expando()]
 
-        personExpando.build = {
-            new Person(personExpando.firstName,
-                    personExpando.lastName,
-                    new Address(addressExpando.streetNumber,
-                            addressExpando.street,
-                            addressExpando.city,
-                            addressExpando.state,
-                            addressExpando.zip,
-                            new PhoneNumber(homePhoneExpando.areaCode, homePhoneExpando.prefix, homePhoneExpando.lineNumber),
-                            new PhoneNumber(mobilePhoneExpando.areaCode, mobilePhoneExpando.prefix, mobilePhoneExpando.lineNumber)))
+        expandos["person"].build = {
+            new Person(expandos["person"].firstName,
+                    expandos["person"].lastName,
+                    new Address(expandos["address"].streetNumber,
+                            expandos["address"].street,
+                            expandos["address"].city,
+                            expandos["address"].state,
+                            expandos["address"].zip,
+                            buildPhoneNumber(expandos["homePhone"]),
+                            buildPhoneNumber(expandos["mobilePhone"])))
         }
+    }
+
+    private buildPhoneNumber(phoneNumberExpando) {
+        new PhoneNumber(phoneNumberExpando.areaCode, phoneNumberExpando.prefix, phoneNumberExpando.lineNumber)
     }
 
     def createNode(Object name, Map attributes) {
-        if ("homePhone" == name) {
-            fillExpandoProperties(homePhoneExpando, attributes)
-        } else if ("mobilePhone" == name) {
-            fillExpandoProperties(mobilePhoneExpando, attributes)
-        } else if ("address" == name) {
-            fillExpandoProperties(addressExpando, attributes)
-        } else if ("person" == name) {
-            fillExpandoProperties(personExpando, attributes)
-        } else {
-            throw new UnsupportedOperationException("${name} is not a supported builder node type!")
-        }
-    }
-
-    private def fillExpandoProperties(Expando expando, Map attributes) {
         attributes.each { key, value ->
-            expando."${key}" = value
+            expandos[name]."${key}" = value
         }
-        expando
+        expandos[name]
     }
 
     void setParent(Object parent, Object child) {
